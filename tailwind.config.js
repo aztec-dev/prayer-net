ba78b367f143884424c3e8d3e5a9927650099560
aztec-dev <aztech.dev1@gmail.com>
2025-11-16 15:08:41 +1000

    feat: added colors variables for light and dark themes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
+  content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+  presets: [require('nativewind/preset')],
   theme: {
+    //
+    colors: {
+      text: 'var(--text)',
+      background: 'var(--background)',
+      primary: 'var(--primary)',
+      secondary: 'var(--secondary)',
+      accent: 'var(--accent)',
+      card: 'var(--card)',
+    },
+
     extend: {},
   },
   plugins: [],
-}
-
+};
