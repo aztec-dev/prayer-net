954d8b0ee6aa70b5b8cd9927062adbb3c1b6b2a4
aztec-dev <aztech.dev1@gmail.com>
2025-11-16 17:52:43 +1000

    feat: added font load useEffect

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,23 @@
-import { Stack } from 'expo-router';
+import { Playfair_400Regular } from '@expo-google-fonts/playfair/400Regular';
+import {
+  Roboto_500Medium,
+  Roboto_800ExtraBold,
+  useFonts,
+} from '@expo-google-fonts/roboto';
+import { SplashScreen, Stack } from 'expo-router';
+import { useEffect } from 'react';
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded, error] = useFonts({
+    Playfair_400Regular,
+    Roboto_500Medium,
+    Roboto_800ExtraBold,
+  });
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
