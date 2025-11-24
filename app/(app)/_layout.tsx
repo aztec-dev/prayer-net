b1c85cc7582586bc5b4e4455b51602de8acfe378
aztec-dev <aztech.dev1@gmail.com>
2025-11-24 22:07:15 +1000

    wip: working on custom tab bar style

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,5 +1,85 @@
-import { Stack } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
+import { useColorScheme } from 'react-native';
 
 export default function AppLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme();
+
+  // Define colors based on theme
+  const colors = {
+    tabBarBackground:
+      colorScheme === 'dark'
+        ? 'rgba(27, 57, 86, 0.8)'
+        : 'rgba(226, 231, 233, 0.8)',
+    activeTab: colorScheme === 'dark' ? '#9dbcfb' : '#1181c8',
+    inactiveTab: colorScheme === 'dark' ? '#fffffa' : '#1a3239',
+  };
+
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          position: 'absolute',
+          bottom: 20,
+          left: 20,
+          right: 20,
+          marginHorizontal: 10,
+          elevation: 0,
+          backgroundColor: colors.tabBarBackground,
+          borderRadius: 20,
+          height: 60,
+          paddingBottom: 8,
+          paddingTop: 8,
+          borderTopWidth: 0,
+          shadowColor: '#000',
+          shadowOffset: { width: 0, height: 10 },
+          shadowOpacity: 0.25,
+          shadowRadius: 20,
+        },
+        tabBarActiveTintColor: colors.activeTab,
+        tabBarInactiveTintColor: colors.inactiveTab,
+        tabBarShowLabel: true,
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: '600',
+        },
+        tabBarItemStyle: {
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      ></Tabs.Screen>
+      <Tabs.Screen
+        name="Prayer"
+        options={{
+          title: 'Prayer Journal',
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="book" size={size} color={color} />
+          ),
+        }}
+      ></Tabs.Screen>
+      <Tabs.Screen
+        name="Profile"
+        options={{
+          title: 'Profile',
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" size={size} color={color} />
+          ),
+        }}
+      ></Tabs.Screen>
+    </Tabs>
+  );
 }
