371c9abe705022f289639c6e662299ef535e40a8
aztec-dev <aztech.dev1@gmail.com>
2025-11-24 16:49:38 +1000

    fix: adjusted height percentage

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,7 @@
 import { Text, View } from 'react-native';
 export default function HeroSection() {
   return (
-    <View className="bg-primary w-full h-[50%] rounded-b-3xl items-center justify-center">
+    <View className="bg-primary w-full h-[40%] rounded-b-3xl items-center justify-center">
       <Text className="text-white">Hero section</Text>
     </View>
   );
