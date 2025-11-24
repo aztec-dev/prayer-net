6a2c41a31078e686a1c7b37c41723a1d8695a49c
aztec-dev <aztech.dev1@gmail.com>
2025-11-24 18:59:55 +1000

    wip: copied signin code as they're similar.

--- app/(auth)/SignUp.tsx
+++ app/(auth)/SignUp.tsx
@@ -1,19 +1,53 @@
 import Buttons from '@/components/Buttons';
+import Card from '@/components/Card';
 import { Ionicons } from '@expo/vector-icons';
 import { useRouter } from 'expo-router';
-import { Text, View } from 'react-native';
+import { TextInput, useColorScheme, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function SignUp() {
+  const colorScheme = useColorScheme();
   const route = useRouter();
+
   return (
-    <View>
-      <Text>Sign Up</Text>
-      <Buttons
-        title="back"
-        onPress={() => route.replace('/')}
-        variant="secondary"
-        icon={<Ionicons name="arrow-back-circle" size={38} color="white" />}
-      ></Buttons>
-    </View>
+    <SafeAreaView className="flex-1 bg-background">
+      <View className="my-auto">
+        <Card title="Sign In" width="w-[90%]">
+          <View className="my-3 self-stretch">
+            <TextInput
+              className="border border-[#ccc] rounded-lg p-6 text-[16px]"
+              placeholder="Email"
+            />
+            <View className="my-3 self-stretch">
+              <TextInput
+                className="border border-[#ccc] rounded-lg p-6 text-[16px]"
+                placeholder="Password"
+                secureTextEntry
+              />
+              <View className="flex flex-row items-center gap-3 mt-10">
+                {/* Back button - retun to index. */}
+                <Buttons
+                  title="back"
+                  onPress={() => route.replace('/')}
+                  icon={
+                    <Ionicons
+                      name="arrow-back-circle"
+                      size={50}
+                      color={colorScheme === 'dark' ? 'white' : 'black'}
+                    />
+                  }
+                />
+                <Buttons
+                  title="Login"
+                  onPress={() => route.replace('/')}
+                  variant="primary"
+                  width="flex-1"
+                />
+              </View>
+            </View>
+          </View>
+        </Card>
+      </View>
+    </SafeAreaView>
   );
 }
