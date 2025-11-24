35d0e05a19e163e5059c6cae9d5595ee074b2caa
aztec-dev <aztech.dev1@gmail.com>
2025-11-24 18:59:25 +1000

    feat: added scroll view for keyboard inputs

--- app/(auth)/SignIn.tsx
+++ app/(auth)/SignIn.tsx
@@ -1,44 +1,100 @@
 import Buttons from '@/components/Buttons';
 import Card from '@/components/Card';
+import { supabase } from '@/lib/supabase';
 import { Ionicons } from '@expo/vector-icons';
 import { useRouter } from 'expo-router';
-import { TextInput, View } from 'react-native';
+import { useState } from 'react';
+import {
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TextInput,
+  useColorScheme,
+  View,
+} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function SignIn() {
+  const colorScheme = useColorScheme();
   const route = useRouter();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  async function AuthUser() {
+    setLoading(true);
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      Alert.alert(error.message);
+    } else if (data?.session) {
+      route.replace('/(app)');
+    }
+    setLoading(false);
+  }
+
   return (
     <SafeAreaView className="flex-1 bg-background">
-      <View className="my-auto">
-        <Card title="Sign In" width="w-[80%]">
-          <Buttons
-            title="back"
-            onPress={() => route.replace('/')}
-            icon={<Ionicons name="arrow-back-circle" size={38} color="white" />}
-          />
-          <View className="my-3 self-stretch">
-            <TextInput
-              className="border border-[#ccc] rounded-lg p-3 text-[16px]"
-              placeholder="Email"
-            />
-            <View className="my-3 self-stretch">
-              <TextInput
-                className="border border-[#ccc] rounded-lg p-3 text-[16px]"
-                placeholder="Password"
-                secureTextEntry
-              />
-            </View>
-            <View className="self-stretch items-end">
-              <Buttons
-                title="Login"
-                onPress={() => route.replace('/')}
-                variant="primary"
-                width="w-[90%]"
-              />
-            </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        className="flex-1"
+      >
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <View className="my-auto">
+            <Card title="Good to See You" width="w-[90%]">
+              <View className="my-3 self-stretch">
+                <TextInput
+                  className="border border-[#ccc] rounded-lg p-6 text-[16px]"
+                  placeholder="Email"
+                  onChangeText={setEmail}
+                  value={email}
+                  autoCapitalize="none"
+                  placeholderTextColor="#ccc"
+                  keyboardType="email-address"
+                />
+                <View className="my-3 self-stretch">
+                  <TextInput
+                    className="border border-[#ccc] rounded-lg p-6 text-[16px]"
+                    placeholder="Password"
+                    onChangeText={setPassword}
+                    value={password}
+                    secureTextEntry
+                    placeholderTextColor="#ccc"
+                    autoCapitalize="none"
+                  />
+                  <View className="flex flex-row items-center gap-3 mt-10">
+                    {/* Back button - retun to index. */}
+                    <Buttons
+                      title="back"
+                      onPress={() => route.replace('/')}
+                      icon={
+                        <Ionicons
+                          name="arrow-back-circle"
+                          size={50}
+                          color={colorScheme === 'dark' ? 'white' : 'black'}
+                        />
+                      }
+                    />
+                    <Buttons
+                      title="Login"
+                      onPress={() => route.replace('/')}
+                      variant="primary"
+                      width="flex-1"
+                    />
+                  </View>
+                </View>
+              </View>
+            </Card>
           </View>
-        </Card>
-      </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
