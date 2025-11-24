a424d1137b924cbe31e7bf877eb58aed3e29f3f8
aztec-dev <aztech.dev1@gmail.com>
2025-11-24 16:49:58 +1000

    wip: working on a custom reusable card

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,13 +2,27 @@ import { Text, View } from 'react-native';
 
 type Props = {
   title: string; // for now, just for the 'sign in' and 'sign up' texts.
+  width?: string;
+  height?: string;
+  textSize?: string;
+  inputLabel?: string;
   children: React.ReactNode; // to pass in any content.
 };
 
-export default function Card({ title, children }: Props) {
+export default function Card({
+  title,
+  width = 'w-auto',
+  height = 'h-auto',
+  textSize = 'text-[16px]',
+  children,
+}: Props) {
   return (
-    <View className="">
-      <Text>Card</Text>
+    <View
+      className={`flex-auto flex-col container p-5 mx-auto bg-card rounded-lg ${textSize} ${width} ${height}`}
+    >
+      <Text className="text-[32px] text-text">{title}</Text>
+      <View>{children}</View>
     </View>
   );
 }
+// container p-5 mx-auto my-auto bg-card rounded-lg flex-col w-[70%]
